997226b09ac5879af857b5006e70d0173d4e0e0e
Matthew Rizzini <matthewrizzini@gmail.com>
2018-02-06 00:09:15 +0000

    completed Udemy Section Two

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,40 +1,40 @@
 // contains all controllers for hotel information
 
 var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId; // requires ObjectId helper. required just this from mongodb
 var hotelData = require('../data/hotel-data.json'); // this brings in the json data from the file into the hotelData var
 
 module.exports.hotelsGetAll = function (req, res) {
   
   var db = dbconn.get(); // saves returned connection object from dbconnection.js as var. connects to the database
   var collection = db.collection('hotels'); // use the database collection & work with collection hotels
-  
+
+  var offset = 0;
+  var count = 5;
+      
+  if (req.query && req.query.offset) { // checks if query property exists and if offset exists
+    offset = parseInt(req.query.offset, 10); // we set offset parameter to a new var. need to turn to a number using parseInt
+    }
+      
+  if (req.query && req.query.count) { // checks if query property exists and if count exists
+    count = parseInt(req.query.count, 10); // we set count parameter to a new var. need to turn to a number using parseInt
+    }
+ 
   collection
     .find() // chaining find method onto collection of hotels
+    .skip(offset) // method to get how many documents to skip
+    .limit(count) // method to get how many doucuments we want to return
     .toArray(function(err, docs) { // turns into an array, and makes it non-blocking
       console.log('Found hotels', docs); // logs the docs 
       res // and prints to browser
         .status(200)
         .json(docs);
    }); 
-  
-
-  
   // console.log("db", db);
   
   // console.log('GET the hotels');
   // console.log(req.query);
-      
-  // var offset = 0;
-  // var count = 5;
-      
-  // if (req.query && req.query.offset) { // checks if query property exists and if offset exists
-  //   offset = parseInt(req.query.offset, 10); // we set offset parameter to a new var. need to turn to a number using parseInt
-  //   }
-      
-  // if (req.query && req.query.count) { // checks if query property exists and if count exists
-  //   count = parseInt(req.query.count, 10); // we set count parameter to a new var. need to turn to a number using parseInt
-  //   }
-      
+
   // var returnData = hotelData.slice(offset, offset+count); // this takes the hotelData array and slicing it and puts it into a new var
 
   // res
@@ -43,19 +43,49 @@ module.exports.hotelsGetAll = function (req, res) {
 };
 
 module.exports.hotelsGetOne = function (req, res) { // conected to the api/hotels/... route
+  var db = dbconn.get(); // saves returned connection object from dbconnection.js as var. connects to the database
+  var collection = db.collection('hotels'); // use the database collection & work with collection hotels
   var hotelId = req.params.hotelId; // finds url parameter when this route is called and assigns to var
-  var thisHotel = hotelData[hotelId]; // holds info about an indiviudal hotel
+  // var thisHotel = hotelData[hotelId]; // holds info about an indiviudal hotel
   console.log('GET hotelId', hotelId);
-  res
+  
+  collection
+  .findOne({
+    _id: ObjectId(hotelId)
+  }, function(err, doc){
+    res
     .status(200)
-    .json( thisHotel ); // 
+    .json( doc ); // 
+  });
+  
+  
 };
 
 
 module.exports.hotelsAddOne = function(req, res) {
+  var db = dbconn.get(); // saves returned connection object from dbconnection.js as var. connects to the database
+  var collection = db.collection('hotels'); // use the database collection & work with collection hotels
+  var newHotel; // storing new var
+  
   console.log("POST new hotel");
-  console.log(req.body);
-  res
-    .status(200)
-    .json(req.body);
+  
+  if (req.body && req.body.name && req.body.stars) { // checks if all 3 of these exist
+    newHotel = req.body; // storing req.body data into newHotel
+    newHotel.stars = parseInt(req.body.stars, 10); // turning this data into a number, instead of a string
+    collection.insertOne(newHotel, function(err, response) { // method to insert the newHotel data. has a callback function
+      console.log(response); // logs entire response
+      console.log(response.ops); // logs document we created, the json. its a property on the response object called ops
+      res
+        .status(201) // status for when a new resource is created
+        .json(response.ops);
+    });
+
+  } else { // if data is missing, this code runs
+    console.log('Data missing from body');
+    res
+    .status(400) // tells browser it was a bad request
+    .json({message: "Required data missing from body"});
+  }
+  
+
 };
--- api/data/dbconnection.js
+++ api/data/dbconnection.js
@@ -1,17 +1,17 @@
 var MongoClient = require('mongodb').MongoClient; // requires driver
 // var dburl = 'mongodb://careerdevs-mwr2017-mrizzini.c9users.io:27017/meanhotel'; //defines connection string
-var dburl =  'mongodb://' + process.env.IP + ':' + '27017/meanhotel'; 
+var dburl =  'mongodb://' + process.env.IP + ':27017'; 
 
 var _connection = null; // private var that will hold the connection
 
 var open = function() { // will eventually use to open the connection
-  MongoClient.connect(dburl, function(err, db) {
+  MongoClient.connect(dburl, function(err, client) {
     if (err) {
-      console.log("DB connection failed " + err);
+      console.log("DB connection failed " + err); 
       return;
     }
-    _connection = db;
-    console.log("DB connection open", db);
+    _connection = client.db("meanhotel"); // changed from _connection = db to this
+    console.log("DB connection open", client);
   });
 };
 
